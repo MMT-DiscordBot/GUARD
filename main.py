9132c461ddd1c227267773610cceb4523b79d8e7
__tseng__ <89698082+tseng-Chen@users.noreply.github.com>
2022-04-30 13:01:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ bot = commands.Bot(command_prefix='g.', intents=discord.Intents.all())
 @bot.event
 async def on_ready() -> None:
     print('-- Bot ready --')
-    for filename in os.listdir('./cogs'):
-        if filename[-3:] == '.py':
-            bot.load_extension(f'cogs.{filename[:-3]}')
+    filename = slice(5, -3)
+    for path in glob.glob('cogs/*.py'):
+        bot.load_extension('cogs.' + path[filename])
 
 if __name__ == '__main__':
     with open('conf.json', mode='r') as f:
